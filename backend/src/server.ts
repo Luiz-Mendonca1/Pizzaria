80cdfaf6eee45469516b045f04829a4638860f0b
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-06-15 21:47:21 -0300

    feat: Arquivo de rotas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,17 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import router from './router';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(cors());
+app.use(router);
 
-const PORT = 3333;
-
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
